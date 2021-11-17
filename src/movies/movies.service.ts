63165ecac6812d0059ba575c99218acf34ee2d4f
BuildTools <unconfigured@null.spigotmc.org>
2021-11-17 13:22:39 +0900

    delete 버그 수정

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -16,9 +16,9 @@ export class MoviesService {
     return movie;
   }
 
-  deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
-    return true;
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
   }
 
   create(movieData) {
@@ -26,5 +26,11 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData,
     });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
