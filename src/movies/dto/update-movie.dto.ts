43b759addc0b4fe25cd165f309f1e67a0ce00dfb
BuildTools <unconfigured@null.spigotmc.org>
2021-11-24 22:20:31 +0900

    보안 업데이트

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { IsNumber, IsString } from 'class-validator';
+
+export class UpdateMovieDto {
+  @IsString()
+  readonly title?: string;
+
+  @IsNumber()
+  readonly year?: number;
+  
+  @IsString({ each: true })
+  readonly genres?: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movies.dto';
 import { Movie } from './entities/Movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -26,22 +27,23 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return this.moviesService.update(movieId, updateData);
   }
 }
